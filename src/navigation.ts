52141ecca8108af16372343df9ee7bedf822e3e0
Rick Robinson <rickrobinson@MacBookPro.hub>
2026-04-11 16:35:12 +1000

    initial travel site upload

---
+++ src/navigation.ts
@@ -0,0 +1,40 @@
+import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+
+export const headerData = {
+  links: [
+    {
+      text: 'Home',
+      href: '/',
+    },
+    {
+      text: 'About',
+      href: '/about',
+    },
+    {
+      text: 'Contact',
+      href: '/contact',
+    },
+    {
+      text: 'Countries',
+      links: [
+        { text: 'Egypt', href: '/egypt' },
+        { text: 'Greece', href: '/greece' },
+        { text: 'Italy', href: '/italy' },
+      ],
+    },
+  ],
+};
+
+export const footerData = {
+  
+  socialLinks: [
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/arthelokyo/astrowind' },
+  ],
+  footNote: `
+    Made by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/arthelokyo"> Arthelokyo</a> · All rights reserved.
+  `,
+};
